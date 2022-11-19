12128547f81725c6b4bd3f8e77dd269e3d441e09
Krushil Naik <krushilnaik96@gmail.com>
2022-11-19 11:06:33 -0500

    Skeleton out routes, add basic styling

--- app.py
+++ app.py
@@ -2,5 +2,25 @@
 Smoothstack Evaluation Week Final Project
 """
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.errorhandler(404)
+def invalid_route(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500)
+def invalid_route(e):
+    return render_template('500.html')
+
+
 if __name__ == "__main__":
     pass
