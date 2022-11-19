439baa81b168e032184200b0772c47913320f855
Krushil Naik <krushilnaik96@gmail.com>
2022-11-19 16:48:27 -0500

    Add file uploading

--- app.py
+++ app.py
@@ -2,17 +2,58 @@
 Smoothstack Evaluation Week Final Project
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, flash, redirect
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
 
+@app.get('/health')
+def check():
+    """
+    Ping this route to make sure the server is up and running
+
+    Returns:
+        str: "OK" if the request successfully hit the server
+    """
+
+    return jsonify({"message": "OK"})
+
+
 @app.route('/')
 def index():
+    """
+    Home page
+
+    Returns:
+        Template: HTML page
+    """
     return render_template('home.html')
+
 
 @app.post("/upload")
 def upload():
+    """
+    POST route to insert Excel data to MySQL
+
+    Returns:
+        _type_: _description_
+    """
+
+    if "file" not in request.files:
+        flash("No file part")
+
+    file = request.files["file"]
+
+    if not file.filename:
+        flash("No file uploaded")
+        return redirect(request.url)
+
+    filename = secure_filename(file.filename)
+
+    file.save(os.path.join('uploads', filename))
+
     return "Upload request received"
 
 
@@ -22,9 +63,5 @@ def invalid_route(e):
 
 
 @app.errorhandler(500)
-def invalid_route(e):
+def server_error(e):
     return render_template('500.html')
-
-
-if __name__ == "__main__":
-    pass
