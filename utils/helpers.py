d0865d2cafb579e37dfcf8a5d992e304ccc4ea49
Krushil Naik <krushilnaik96@gmail.com>
2022-11-21 15:36:33 -0500

    Clean up code

--- utils/helpers.py
+++ utils/helpers.py
@@ -96,41 +96,6 @@ def has_been_parsed(filename):
         return False
 
 
-# def validate_and_write(values: list):
-#     """
-#     Write data to MySQL upon successful validation
-
-#     Args:
-#         values (list): [time_period, calls_offered, abandoned_after_30, fcr, dsat, csat]
-#     """
-
-#     # run each row through the validator
-#     SummaryValidator(
-#         time_period=values[0],
-#         calls_offered=values[1],
-#         abandoned_after_30=values[2],
-#         fcr=values[3],
-#         dsat=values[4],
-#         csat=values[5]
-#     )
-
-#     # load validated data into ORM
-#     row = Summary(
-#         time_period=values[0],
-#         calls_offered=values[1],
-#         abandoned_after_30=values[2],
-#         fcr=values[3],
-#         dsat=values[4],
-#         csat=values[5],
-#     )
-
-#     # write row to mysql
-#     db.session.add(row)
-#     db.session.commit()
-
-#     return row.as_dict()
-
-
 def file_to_archives(filename):
     """
     Move a file from uploads folder to archive folder
